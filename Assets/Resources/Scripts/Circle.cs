5963e4d2d0497163aaf124b071d44c97336766bc
Dave Yan <fdu5526@live.com>
2016-01-31 15:57:09 -0800

    added all the texts and colors

--- Assets/Resources/Scripts/Circle.cs
+++ Assets/Resources/Scripts/Circle.cs
@@ -10,19 +10,27 @@ public class Circle : MonoBehaviour {
 	Rigidbody2D rigidbody2d;
 	Collider2D collider2d;
 
+	GameObject display;
+
 	// Use this for initialization
 	void Start () {
 		rigidbody2d = GetComponent<Rigidbody2D>();
 		collider2d = GetComponent<Collider2D>();
-		scale = transform.localScale.x;
+		scale = UnityEngine.Random.Range(0.1f, 1f);
+		display = transform.Find("Display").gameObject;
+		transform.position = new Vector3(transform.position.x, transform.position.y, -scale);
+		transform.localScale = new Vector2(scale, scale);
+
 	}
 
 	void ChangeScale (float ds) {
 		scale += ds;
 		transform.localScale = new Vector2(scale, scale);
+		transform.position = new Vector3(transform.position.x, transform.position.y, -scale);
 	}
 	 
 	// Update is called once per frame
 	void Update () {
+
 	}
 }
