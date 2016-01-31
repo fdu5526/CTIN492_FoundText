c6f6b0f49cfe4844f7ca9e1eed1b11a9afc1dea9
Dave Yan <fdu5526@live.com>
2016-01-31 00:57:27 -0800

    added me some prefabs

---
+++ Assets/Resources/Scripts/Circle.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Rigidbody2D))]
+[RequireComponent (typeof (Collider2D))]
+public class Circle : MonoBehaviour {
+
+	Rigidbody2D rigidbody2d;
+	Collider2D collider2d;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody2d = GetComponent<Rigidbody2D>();
+		collider2d = GetComponent<Collider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
