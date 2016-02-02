4d12808f867a512e40bc82f8648f506c5d2fbc90
Dave Yan <fdu5526@live.com>
2016-02-01 17:26:56 -0800

    starting hurt UI

---
+++ Assets/Resources/Scripts/HealthIndicator.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HealthIndicator : MonoBehaviour {
+
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+
+
+	public void TurnOn () {
+		animator.SetBool("On", true);
+	}
+
+	public void TurnOff () {
+		animator.SetBool("On", false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
